25fb5cb60b97f13cebeba0d3ce4fc7e232821754
Asa Grace <asag182@gmail.com>
2021-09-11 14:54:56 -0600

    tests for Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
     const categories = [
@@ -15,14 +16,14 @@ function Nav() {
     return(
         <header data-testid="header" className="flex-row px-1">
             <h2>
-                <a href="/">
+                <a data-testid="link" href="/">
                     <span role="img" aria-label="camera">📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about" onClick={() => handleClick()}>
+                        <a data-testid="about" href="#about" onClick={() => handleClick()}>
                             About me
                         </a>
                     </li>
